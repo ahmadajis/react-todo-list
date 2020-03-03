5ee6bd6ba2750c5e98bdc2d1d326ea5469a98aa2
Ahmad Aji Santoso <49109789+ahmadajis@users.noreply.github.com>
2020-03-03 22:07:20 +0700

    Initial

--- src/App.js
+++ src/App.js
@@ -1,11 +1,77 @@
 import React from "react";
 import "./styles.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="container">
+      <div className="mt-0">
+        <section className="d-flex justify-content-between shadow-none p-3 mb-5 bg-light rounded ">
+          <button type="button" className="btn btn-info">
+            Add Todo
+          </button>
+          <h1 className="header-title text-center ">React Todo Lists</h1>
+          <button type="button" className="btn btn-danger">
+            Clear Todo
+          </button>
+        </section>
+
+        <section className="shadow p-3 mb-5 gradient1 rounded">
+          <br />
+          <div className="container bg-light rounded-lg">
+            <br />
+            <form>
+              <div className="form-row align-items-center">
+                <div className="col-sm-11 my-1">
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="inputTodo"
+                    placeholder="Todo "
+                  />
+                </div>
+                <div className="col-auto my-1">
+                  <button
+                    type="submit"
+                    className="btn btn-dark gdbtn shadow-sm "
+                  >
+                    Submit
+                  </button>
+                </div>
+              </div>
+            </form>
+            <br />
+          </div>
+
+          <br />
+          <hr />
+          <br />
+
+          <div className="todo shadow-sm p-3 mb-3 bg-white rounded ">
+            <span className="todo-list">Learning React Todo </span>
+          </div>
+
+          <div className="todo shadow-sm p-3 mb-3 bg-white rounded ">
+            <span className="todo-list">Learning React Todo </span>
+          </div>
+
+          <div className="todo shadow-sm p-3 mb-3 bg-white rounded ">
+            <span className="todo-list">Learning React Todo </span>
+          </div>
+
+          <div className="todo shadow-sm p-3 mb-3 bg-white rounded ">
+            <span className="todo-list">Learning React Todo </span>
+          </div>
+
+          <div className="todo shadow-sm p-3 mb-3 bg-white rounded ">
+            <span className="todo-list">Learning React Todo </span>
+          </div>
+
+          <div className="todo shadow-sm p-3 mb-3 bg-white rounded ">
+            <span className="todo-list">Learning React Todo </span>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
